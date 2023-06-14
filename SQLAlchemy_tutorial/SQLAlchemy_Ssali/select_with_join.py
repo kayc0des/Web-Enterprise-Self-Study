a7230036c05ce85dd46ed037e8e6b6103ba787e2
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2023-06-14 21:59:17 +0200

    deleting

---
+++ SQLAlchemy_tutorial/SQLAlchemy_Ssali/select_with_join.py
@@ -0,0 +1,12 @@
+from main import session
+from models import User, Comment
+from sqlalchemy import select
+
+statement = select(Comment).join(Comment.user).where(
+    User.username == 'Kingsley'
+).where(
+    Comment.text == 'Hello World'
+)
+
+result = session.scalars(statement).one()
+print(result)
