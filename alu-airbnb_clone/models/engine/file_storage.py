37d848b2cd45368646f993200f718ba299588ebc
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2023-06-15 10:27:43 +0200

    file storage

--- alu-airbnb_clone/models/engine/file_storage.py
+++ alu-airbnb_clone/models/engine/file_storage.py
@@ -45,3 +45,42 @@ class FileStorage():
                     self.new(eval(class_name + "(**" + str(item) + ")"))
         except FileNotFoundError:
             pass
+
+
+"""Alternative"""
+class FileSt():
+    """Serializes instances to JSON file and
+    Deserializes JSON file to instances"""
+
+    __file_path = 'file.json'
+    __objects = dict()
+
+    def all(self):
+        """Returns the dictionary object"""
+        return self.__objects
+    
+    def new(self, obj):
+        """Adds to the object dictionary a new obj by <class name>.id"""
+        
+        classname = obj.__class__name
+        id = obj.id
+        key = f"{classname}.{id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """serializes __object to the JSON file"""
+
+        #convert object to JSON-formatted String
+        json_data = json.dump(self.__objects)
+
+        #open a file in write mode
+        with open(self.__file_path, 'w') as file:
+            file.write(json_data)
+
+    def reload(self):
+        """Deserialize JSON file to __objects"""
+
+        with open(self.__file_path, 'r') as file:
+            json_data = file.read()
+
+        self.__objects = json.loads(json_data)
