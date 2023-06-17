99da2cde2dea6ed1f570c3e7035f8d3cfc132d76
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2023-06-17 13:48:47 +0200

    console

--- alu-airbnb_clone/console.py
+++ alu-airbnb_clone/console.py
@@ -1,6 +1,7 @@
 import cmd 
-import models
+import sys
 from models.base_model import BaseModel
+#from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -8,13 +9,13 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = '(hbnb) '
 
-    def do_quit(self, line):
+    def do_quit(self, arg):
         """
         Exits the command lop
         """
         return True
 
-    def do_EOF(self, line):
+    def do_EOF(self, arg):
         """
         Exit the program
         """
@@ -24,11 +25,13 @@ class HBNBCommand(cmd.Cmd):
         """ When no input is passed"""
         pass
 
-    def do_create(self, line):
+    def do_create(self, arg):
         """
         Create a new instance of BaseModel
         """
+
         
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
